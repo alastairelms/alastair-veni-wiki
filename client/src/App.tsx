02eda85505407529b17300c4f635e6c6a7c22897
Alastair Elms <elms.alastair@gmail.com>
2022-06-21 16:49:37 +0100

    refactor: removed redundant code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,11 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import Searchbar from './components/SearchBar';
 import MoviesView from './components/MoviesView';
-import React, { useState } from 'react';
 
 import "./App.css"
 
 function App() {
-
-    const [films, setFilms] = useState({
-        Title: "",
-        Rated: "",
-        Poster: "",
-        Year: ""
-    });
 
     return (
         <div className="App">
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import "../styles/Navbar.css"
 const Navbar = () => {
     return (
         <div className="navbar">
+            <h2>Star Wars Wiki</h2>
             <div className="nav-links">
                 <a href="#">Home</a>
-                <a href="#">My Watchlist</a>
             </div>
         </div>
     )
