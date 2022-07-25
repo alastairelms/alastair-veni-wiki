3afc6ba64ffda90e4b70728962966494b8683099
venialjo <veni.aljo@accenture.com>
2022-07-25 12:15:16 +0100

    touch up

--- client/src/pages/PlanetsShowPage.tsx
+++ client/src/pages/PlanetsShowPage.tsx
@@ -37,12 +37,7 @@ export const PlanetsShowPage = () => {
     if (filmsLoading || planetsLoading) return <Spinner />;
 
     const planet = planetsData.allPlanets.planets.find((planet: Planet) => planet.id === id)
-    console.log(planet)
-    console.log(planet.filmConnection.films)
-
     const filmList = filmsData.allFilms.films
-    console.log(filmList)
-
     const filmIDs = filmList.map((film: Film) => film.id)
 
     const filmsToShow = planet.filmConnection.films.map((film: Film) => {
@@ -50,7 +45,6 @@ export const PlanetsShowPage = () => {
             return filmList.find((filmData: Film) => filmData.id === film.id)
         }
     })
-    console.log("films with planet!", filmsToShow)
 
     return (
 
